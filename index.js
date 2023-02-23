9cc0b01eab05cc2c692ed184a886fc41a40bc7d0
Michael Jeung <michaelj229@gmail.com>
2023-02-23 11:49:11 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+const employee = {
+    name: "Michael",
+    streetAddress: "363 Windham Loop"
+};
+function updateEmployeeWithKeyAndValue(employee, streetAddress) {
+    return {
+        ...employee,
+        [streetAddress]: "11 Broadway"
+    };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress) {
+    employee[streetAddress] = "12 Broadway";
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, name) {
+    return {
+        ...employee,
+        [name]: undefined
+    };
+}
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee.name;
+    return employee;
+}
